68963c3c37b0567ad54ed934ec2fc05d2541de94
akazwz <akazwz@pm.me>
2022-08-08 10:53:23 +0800

    update path

--- grpc/proto.ts
+++ grpc/proto.ts
@@ -3,19 +3,18 @@ const grpc = require('@grpc/grpc-js')
 const protoLoader = require('@grpc/proto-loader')
 
 export const loadProto = (name: string) => {
-  const PROTO_PATH = path.join(__dirname + '/protos/', `${name}.proto`)
-  console.log(PROTO_PATH)
-  const options = {
-    keepCase: true,
-    longs: String,
-    enums: String,
-    defaults: true,
-    oneofs: true,
-  }
+	const PROTO_PATH = path.join('../protos/', `${name}.proto`)
+	const options = {
+		keepCase: true,
+		longs: String,
+		enums: String,
+		defaults: true,
+		oneofs: true,
+	}
 
-  const packageDefinition = protoLoader.loadSync(PROTO_PATH, options)
-  const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
-  return protoDescriptor[name]
+	const packageDefinition = protoLoader.loadSync(PROTO_PATH, options)
+	const protoDescriptor = grpc.loadPackageDefinition(packageDefinition)
+	return protoDescriptor[name]
 }
 
 
